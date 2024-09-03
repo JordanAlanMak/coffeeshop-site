9a9dc3586a3634f1065b07ea4c889152589932f2
Jordan Mak <JordanAlanMak@gmail.com>
2024-09-02 19:47:20 -0700

    Mobile nav feature/functionality

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ const scrollElements = Array.from(document.querySelectorAll('.to-fade-in'))
 const scrollOffset = 5
 
 const handleElementFadeIn = () => {
-    // Check if elements exist
     scrollElements.forEach(item => {
         const elTop = item.getBoundingClientRect().top
         
@@ -19,37 +18,70 @@ const handleElementFadeIn = () => {
 const nav = document.querySelector('#main-nav')
 let prevScrollY = 0
 
-const handleNavScroll = (e) => {
+const handleNavScroll = () => {
     let curScrollY = window.scrollY || window.pageYOffset
-    
+
     // document.activeElement used for accessibility if nav item is currently focused
     if (curScrollY > prevScrollY && !nav.contains(document.activeElement)) {
         nav.classList.add('nav-hidden')
         nav.classList.remove('nav-visible')
-    } else {
+    // scrolling up more than 100px
+    } else if (curScrollY <= prevScrollY - 100) {
         nav.classList.add('nav-visible')
         nav.classList.remove('nav-hidden')
     }
     prevScrollY = curScrollY
 };
 
-/* IIFE: Create event handlers that call scrolling functions
-***********************************************************/
+/* Create event handlers that call scrolling functions
+*****************************************************/
 ['scroll', 'scrollend', 'load'].forEach(event => {
-    const timeout = 300
     let throttleEvent = false
 
     window.addEventListener(event, () => {
+        const timeout = 250
         if (throttleEvent) return
         throttleEvent = true
-        
+
         handleElementFadeIn()
-        if (event === 'scroll' || (event === 'scrollend' && nav.classList.contains('nav-hidden'))) {
-            handleNavScroll(event)
+        if
+            (event === 'scroll' ||
+            (event === 'scrollend' && (nav.classList.contains('nav-hidden') || window.scrollY < 500)
+            ))
+        {
+            handleNavScroll()
         }
+        
         setTimeout(() => {
             throttleEvent = false
         }, timeout)
-        console.log('finished running')
     })
 });
+
+/* Mobile Nav
+*****************************************************/
+// Menu Toggle
+const menuToggle = document.querySelector('#menu-toggle')
+const sideNav = document.querySelector('#side-nav')
+
+menuToggle.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    if (sideNav.classList.contains('hide')) {
+        sideNav.classList.remove('hide')
+        document.body.classList.add('side-nav-open')
+    } else {
+        sideNav.classList.add('hide')
+        document.body.classList.remove('side-nav-open')
+    }
+})
+
+// Handle click on mobile link
+const menuLinks = document.querySelectorAll('.side-nav-link')
+
+menuLinks.forEach((item) => {
+    item.addEventListener('click', () => {
+        sideNav.classList.add('hide')
+        document.body.classList.remove('side-nav-open')
+    })
+})
